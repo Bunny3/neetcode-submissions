e29199f2a86b82a135403e4acaf7deab5a68bccf
Shubham Agarwal <31550031+Bunny3@users.noreply.github.com>
2026-05-31 15:30:54 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int count[] = new int[26];
+        Arrays.fill(count,0);
+        
+        for(int i=0;i<s1.length();i++) {
+            count[s1.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<=s2.length()-s1.length();i++) {
+            // Get Window of length of S1
+            int start = i;
+            int end = i+s1.length()-1;
+            int current[] = new int[26];
+            Arrays.fill(current,0);
+            while(start<=end) {
+                char ch = s2.charAt(start);
+                current[ch-'a']++;
+                start++;
+            }
+            //Check if the character matches in both running count
+            boolean temp = true;
+            for(int j=0;j<26;j++) {
+                if(current[j]!=count[j])
+                    temp = false;
+            }
+            if(temp == true)
+                return true;
+            
+        }
+        return false;
+    }
+}
